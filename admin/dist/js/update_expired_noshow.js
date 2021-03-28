95ba224378bfeb3c74c992b6246fffd3522dc9b7
Rauf Dimaampao <zrsdimaampao@globe.com.ph>
2021-03-28 21:04:46 +0800

    revision 1

---
+++ admin/dist/js/update_expired_noshow.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $.ajax({
+        url: "../../php_function/expired_noshow/update_booking.php",
+        type: "POST",
+        success: function (response) {
+
+        },
+    });
+
+
+});
