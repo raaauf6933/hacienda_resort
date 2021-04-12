6694511a365f4574878988fdb8fec181cf1dc4cd
Rauf Dimaampao <adimaampao@stratpoint.com>
2021-04-12 11:46:34 +0800

    edit update expired

--- admin/dist/js/update_expired_noshow.js
+++ admin/dist/js/update_expired_noshow.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $.ajax({
-        url: "../../php_function/expired_noshow/update_booking.php",
+        url: "../php_function/expired_noshow/update_booking.php",
         type: "POST",
         success: function (response) {
 
